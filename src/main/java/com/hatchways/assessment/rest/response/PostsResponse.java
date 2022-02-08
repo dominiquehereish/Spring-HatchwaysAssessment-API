b58303a785b60e73ff5152782f68e023bb21bf88
unknown <ha291921@ens.uqam.ca>
2022-02-07 19:36:07 -0500

    posts response

--- src/main/java/com/hatchways/assessment/rest/response/PostsResponse.java
+++ src/main/java/com/hatchways/assessment/rest/response/PostsResponse.java
@@ -1,5 +1,16 @@
 package com.hatchways.assessment.rest.response;
 
+import com.hatchways.assessment.model.Post;
+
+import java.util.List;
+
 public class PostsResponse extends Response{
 
+    private List<Post> posts;
+
+    public List<Post> getPosts(){return posts;}
+
+    public void setPosts(List<Post> list){
+        this.posts = list;
+    }
 }
