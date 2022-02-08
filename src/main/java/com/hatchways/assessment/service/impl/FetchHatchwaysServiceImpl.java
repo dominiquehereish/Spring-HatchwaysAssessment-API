7527335cfece7b70e9f0c9bc523d5193556fcecb
unknown <ha291921@ens.uqam.ca>
2022-02-07 19:45:20 -0500

    make http request method

--- src/main/java/com/hatchways/assessment/service/impl/FetchHatchwaysServiceImpl.java
+++ src/main/java/com/hatchways/assessment/service/impl/FetchHatchwaysServiceImpl.java
@@ -3,10 +3,22 @@ package com.hatchways.assessment.service.impl;
 import com.hatchways.assessment.rest.response.PingResponse;
 import com.hatchways.assessment.rest.response.PostsResponse;
 import com.hatchways.assessment.service.FetchHatchwaysService;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 public class FetchHatchwaysServiceImpl implements FetchHatchwaysService {
+
+    final String HATCHWAYS_URL_API = "https://api.hatchways.io/assessment/blog/posts";
+
     @Override
     public PostsResponse getPosts(String tag, String sortBy, String direction) {
         return null;
@@ -14,6 +26,19 @@ public class FetchHatchwaysServiceImpl implements FetchHatchwaysService {
 
     @Override
     public PingResponse getPing() {
+        return null;
+    }
+
+    private CloseableHttpResponse performGetRequest(String params) {
+        try {
+            HttpGet request = new HttpGet(HATCHWAYS_URL_API);
+            URI uri = new URIBuilder(request.getURI()).addParameter("tag", params).build();
+            request.setURI(uri);
+            CloseableHttpClient client = HttpClients.createDefault();
+            return client.execute(request);
+        } catch (URISyntaxException e) {
+        } catch (IOException e){
+        }
         return null;
     }
 }
