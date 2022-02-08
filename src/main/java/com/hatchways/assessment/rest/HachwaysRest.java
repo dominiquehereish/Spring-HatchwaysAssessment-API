50a02dc26e2bfc0592c0a70b47a0496b49aaab06
unknown <ha291921@ens.uqam.ca>
2022-02-07 20:13:24 -0500

    /posts

--- src/main/java/com/hatchways/assessment/rest/HachwaysRest.java
+++ src/main/java/com/hatchways/assessment/rest/HachwaysRest.java
@@ -1,7 +1,11 @@
 package com.hatchways.assessment.rest;
 
+import com.hatchways.assessment.rest.response.ErrorResponse;
 import com.hatchways.assessment.rest.response.PingResponse;
+import com.hatchways.assessment.rest.response.PostsResponse;
 import com.hatchways.assessment.rest.response.Response;
+import com.hatchways.assessment.rest.values.DirectionValues;
+import com.hatchways.assessment.rest.values.SortByValues;
 import com.hatchways.assessment.service.FetchHatchwaysService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +31,19 @@ public class HachwaysRest {
     public ResponseEntity<Response> getPosts(@RequestParam(required = true) String tags,
                                             @RequestParam(defaultValue = "id") String sortBy,
                                             @RequestParam(defaultValue = "asc") String direction){
-        return null;
+
+        if(SortByValues.fromString(sortBy.toUpperCase()) == null)
+        {
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.setError("sortBy parameter is invalid");
+            return ResponseEntity.badRequest().body(errorResponse);
+        }else if(DirectionValues.fromString(direction) == null){
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.setError("direction parameter is invalid");
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+
+        PostsResponse postsResponse = fetchHatchwaysService.getPosts(tags, sortBy, direction);
+        return ResponseEntity.ok().body(postsResponse);
     }
 }
