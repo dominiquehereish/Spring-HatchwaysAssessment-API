1ff519eada0861519960d27966c3caf40c6eae1a
unknown <ha291921@ens.uqam.ca>
2022-02-07 19:35:01 -0500

    post model

---
+++ src/main/java/com/hatchways/assessment/model/Post.java
@@ -0,0 +1,71 @@
+package com.hatchways.assessment.model;
+
+import java.util.List;
+
+public class Post {
+
+    private String author;
+    private int authorId;
+    private long id;
+    private int likes;
+    private double popularity;
+    private long reads;
+    private List<String> tags;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public long getReads() {
+        return reads;
+    }
+
+    public void setReads(long reads) {
+        this.reads = reads;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+}
