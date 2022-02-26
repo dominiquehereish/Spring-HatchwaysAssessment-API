3b619cc5dafba9a0d5d69e669fa1dfa7e224c643
unknown <ha291921@ens.uqam.ca>
2022-02-26 13:47:39 -0500

    tests

--- src/test/java/com/hatchways/assessment/AssessmentApplicationTests.java
+++ src/test/java/com/hatchways/assessment/AssessmentApplicationTests.java
@@ -1,13 +1,17 @@
 package com.hatchways.assessment;
 
+import com.hatchways.assessment.rest.HatchwaysRest;
+import com.hatchways.assessment.service.FetchHatchwaysService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AssessmentApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
     }
 
 }
